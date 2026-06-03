103f8556daf5959dd09a5541cb29ac796b3c5fb1
Luca Meriano <luca@meriano.ch>
2026-06-03 19:25:44 +0200

    PARTICIPANTS: using generated price

--- participants.py
+++ participants.py
@@ -4,7 +4,7 @@ from sortedcontainers import SortedDict
 from start import match_order, cancel_limit_order
 
 
-def place_price(type, bids, asks, ref):
+def generate_price(type, bids, asks, ref):
     if type == "buy":
         anchor = bids.peekitem(-1)[0] if bids else ref  # best bid
     elif type == "sell":
@@ -34,27 +34,17 @@ choices = [
     "cancel_sell_limit",
 ]
 probabilities = [0.10, 0.25, 0.25, 0.08, 0.08, 0.12, 0.12]
+ref = 100
 
 for _ in range(decisions):
-    # CHOICES: 0-7
-    # do nothing
-    # buy limit order
-    # sell limit order
-    # buy market order
-    # sell market order
-    # cancel buy limit order
-    # cancel sell limit order
+    choice = np.random.choice(choices, p=probabilities)
+    quantity = np.random.choice(range(1, 100))
 
-    choice = np.random.choice(range(0, 7))
-
-    # do nothing
-    if choice == 0:
+    if choice == "nothing":
         trades = []
 
-    # buy limit order
-    elif choice == 1:
-        price = 101
-        quantity = np.random.choice(range(1, 100))
+    elif choice == "buy_limit":
+        price = generate_price("buy", bids, asks, ref)
         trades, remaining = match_order("buy", price, quantity, bids, asks)
         print(trades)
 
@@ -63,10 +53,8 @@ for _ in range(decisions):
             # get(..., 0) avoids a KeyError
             bids[price] = bids.get(price, 0) + remaining
 
-    # sell limit order
-    elif choice == 2:
-        price = 101
-        quantity = np.random.choice(range(1, 100))
+    elif choice == "sell_limit":
+        price = generate_price("sell", bids, asks, ref)
         trades, remaining = match_order("sell", price, quantity, bids, asks)
         print(trades)
 
@@ -74,33 +62,25 @@ for _ in range(decisions):
         if remaining > 0:
             asks[price] = asks.get(price, 0) + remaining
 
-    # buy market order
-    elif choice == 3:
-        quantity = np.random.choice(range(1, 100))
+    elif choice == "buy_market":
         market_order = True
         trades, unfilled = match_order(
             "buy", 0, quantity, bids, asks, market_order=market_order
         )
 
-    # sell market order
-    elif choice == 4:
-        quantity = np.random.choice(range(1, 100))
+    elif choice == "sell_market":
         market_order = True
         trades, unfilled = match_order(
             "sell", 0, quantity, bids, asks, market_order=market_order
         )
 
-    # cancel buy limit order
-    elif choice == 5:
-        price = 101
-        quantity = np.random.choice(range(1, 100))
+    elif choice == "cancel_buy_limit":
+        price = generate_price("buy", bids, asks, ref)
         cancel_limit_order("buy", price, quantity, bids, asks)
         trades = []
 
-    # cancel sell limit order
-    elif choice == 6:
-        price = 101
-        quantity = np.random.choice(range(1, 100))
+    elif choice == "cancel_sell_limit":
+        price = generate_price("sell", bids, asks, ref)
         cancel_limit_order("sell", price, quantity, bids, asks)
         trades = []
 
