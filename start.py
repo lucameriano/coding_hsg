475d3fd20cb6e544aa2b88bf128d5439fe7a40da
Luca Meriano <luca@meriano.ch>
2026-06-03 17:07:56 +0200

    LOB more realistic

--- start.py
+++ start.py
@@ -10,15 +10,46 @@ asks[100] = 10
 print(bids)
 print(asks)
 
-highest_bid = bids.peekitem(-1)
-lowest_ask = asks.peekitem(0)
-print(highest_bid[0])
-print(lowest_ask[0])
-print("-" * 40)
+#######################################################
 
-for bid_price, bid_quantity in bids.items():
-    print(bid_price, bid_quantity)
-    for ask_price, ask_quantity in asks.items():
-        print(ask_price, ask_quantity)
-        if bid_price >= ask_price:
-            print("Match found")
+# Slow:
+# Match
+# for bid_price, bid_quantity in bids.items():
+#     print(bid_price, bid_quantity)
+#     for ask_price, ask_quantity in asks.items():
+#         print(ask_price, ask_quantity)
+#         if bid_price >= ask_price:
+#             print("Match found")
+
+#######################################################
+
+
+# Match incoming order
+# Inputs: type, limit_price, quantity, bids, asks
+
+trades = []
+type = "buy"
+
+if type == "buy":
+    book = asks
+elif type == "sell":
+    book = bids
+
+# Highest bid and lowest ask
+bid_price, bid_qty = bids.peekitem(-1)
+ask_price, ask_qty = asks.peekitem(0)
+
+# Only the best bid and ask can cross
+# Always match top-against-top, and re-check the top after every fill.
+# The top crossing is your only signal to continue; the top not crossing is a complete proof that you're done.
+if bid_price < ask_price:
+    print("No match")
+
+# If the above condition is false, we have a match
+# So bid_price >= ask_price
+print("Match found")
+
+# Trade happens and the lowest demand gets exchanged
+trade_quantity = min(bid_qty, ask_qty)
+
+# Price of the trade depends on who the aggressive and passive order was
