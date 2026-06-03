868ce56893fa3b66ea877750851c2b99a4181ce5
Luca Meriano <luca@meriano.ch>
2026-06-03 18:10:07 +0200

    if __name__ == "__main__" added

--- start.py
+++ start.py
@@ -1,10 +1,5 @@
 from sortedcontainers import SortedDict
 
-bids = SortedDict({99: 10, 99.5: 10, 100: 10})
-asks = SortedDict({102: 10, 101.5: 10, 101: 10})
-# bids = SortedDict({})
-# asks = SortedDict({})
-
 
 def match_order(type, limit_price, quantity, bids, asks, market_order=False):
     trades = []
@@ -58,44 +53,52 @@ def match_order(type, limit_price, quantity, bids, asks, market_order=False):
     return trades, quantity
 
 
-# Limit order
-"""
-The key feature of this order type is that you  provide a limit price which is the worst price you are willing to trade at. 
-When you trade you may get a slightly better price than your limit price, depending on the structure of the book.
-Source: https://www.machow.ski/posts/2021-07-18-introduction-to-limit-order-books
-"""
+if __name__ == "__main__":
+    bids = SortedDict({99: 10, 99.5: 10, 100: 10})
+    asks = SortedDict({102: 10, 101.5: 10, 101: 10})
 
-type = "buy"
-# type = "sell"
-price = 101
-quantity = 2
-trades, remaining = match_order(type, price, quantity, bids, asks)
-print(trades)
+    # bids = SortedDict({})
+    # asks = SortedDict({})
 
-# Rest the remaining part of the order, add it to the relevant book
-if remaining > 0:
-    if type == "buy":
-        # get(..., 0) avoids a KeyError
-        bids[price] = bids.get(price, 0) + remaining
-    if type == "sell":
-        asks[price] = asks.get(price, 0) + remaining
-print(bids)
-print(asks)
-print("#" * 40)
+    # Limit order
+    """
+    The key feature of this order type is that you  provide a limit price which is the worst price you are willing to trade at. 
+    When you trade you may get a slightly better price than your limit price, depending on the structure of the book.
+    Source: https://www.machow.ski/posts/2021-07-18-introduction-to-limit-order-books
+    """
 
+    type = "buy"
+    # type = "sell"
+    price = 101
+    quantity = 2
+    trades, remaining = match_order(type, price, quantity, bids, asks)
+    print(trades)
 
-# Market order
-"""
-A market order is a special order type that does not require you to provide a limit price. 
-It is an instruction to buy or sell a certain quantity of shares at any price available. 
-If a market Buy order is submitted to the exchange, the exchange will start matching against orders on the 
-ask side of the book regardless of the price until the order is filled, or there is no more quantity remaining.
-Source: https://www.machow.ski/posts/2021-07-18-introduction-to-limit-order-books
-"""
+    # Rest the remaining part of the order, add it to the relevant book
+    if remaining > 0:
+        if type == "buy":
+            # get(..., 0) avoids a KeyError
+            bids[price] = bids.get(price, 0) + remaining
+        if type == "sell":
+            asks[price] = asks.get(price, 0) + remaining
+    print(bids)
+    print(asks)
+    print("#" * 40)
 
-market_order = True
-type = "buy"
-# type = "sell"
-quantity = 22
-trades, unfilled = match_order(type, 0, quantity, bids, asks, market_order=market_order)
-print(trades)
+    # Market order
+    """
+    A market order is a special order type that does not require you to provide a limit price. 
+    It is an instruction to buy or sell a certain quantity of shares at any price available. 
+    If a market Buy order is submitted to the exchange, the exchange will start matching against orders on the 
+    ask side of the book regardless of the price until the order is filled, or there is no more quantity remaining.
+    Source: https://www.machow.ski/posts/2021-07-18-introduction-to-limit-order-books
+    """
+
+    market_order = True
+    type = "buy"
+    # type = "sell"
+    quantity = 22
+    trades, unfilled = match_order(
+        type, 0, quantity, bids, asks, market_order=market_order
+    )
+    print(trades)
